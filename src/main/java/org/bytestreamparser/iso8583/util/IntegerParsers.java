cfd85d0555d4a64d5567606a63f25eb0a1e6ccfc
Lin Yang <coding@lyang.dev>
2025-01-17 23:11:37 -0800

    Minor tweaks

--- src/main/java/org/bytestreamparser/iso8583/util/IntegerParsers.java
+++ src/main/java/org/bytestreamparser/iso8583/util/IntegerParsers.java
@@ -1,18 +1,18 @@
 package org.bytestreamparser.iso8583.util;
 
 import org.bytestreamparser.api.data.Data;
-import org.bytestreamparser.api.parser.DataParser;
+import org.bytestreamparser.scalar.parser.NumberParser;
 import org.bytestreamparser.scalar.parser.UnsignedByteParser;
 import org.bytestreamparser.scalar.parser.UnsignedShortParser;
 
 public final class IntegerParsers {
   private IntegerParsers() {}
 
-  public static <P extends Data<P>> DataParser<P, Integer> ubyte(String id) {
+  public static <P extends Data<P>> NumberParser<P, Integer> ubyte(String id) {
     return new UnsignedByteParser<>(id);
   }
 
-  public static <P extends Data<P>> DataParser<P, Integer> ushort(String id) {
+  public static <P extends Data<P>> NumberParser<P, Integer> ushort(String id) {
     return new UnsignedShortParser<>(id);
   }
 }
--- src/main/java/org/bytestreamparser/iso8583/util/StringParsers.java
+++ src/main/java/org/bytestreamparser/iso8583/util/StringParsers.java
@@ -2,28 +2,27 @@ package org.bytestreamparser.iso8583.util;
 
 import java.nio.charset.Charset;
 import org.bytestreamparser.api.data.Data;
-import org.bytestreamparser.api.parser.DataParser;
 import org.bytestreamparser.scalar.parser.BcdStringParser;
 import org.bytestreamparser.scalar.parser.CharStringParser;
 import org.bytestreamparser.scalar.parser.HexStringParser;
+import org.bytestreamparser.scalar.parser.StringParser;
 
 public final class StringParsers {
   private StringParsers() {}
 
-  public static <P extends Data<P>> DataParser<P, String> plain(String id, int length) {
+  public static <P extends Data<P>> StringParser<P> plain(String id, int length) {
     return plain(id, length, Charset.defaultCharset());
   }
 
-  public static <P extends Data<P>> DataParser<P, String> plain(
-      String id, int length, Charset charset) {
+  public static <P extends Data<P>> StringParser<P> plain(String id, int length, Charset charset) {
     return new CharStringParser<>(id, length, charset);
   }
 
-  public static <P extends Data<P>> DataParser<P, String> hex(String id, int length) {
+  public static <P extends Data<P>> StringParser<P> hex(String id, int length) {
     return new HexStringParser<>(id, length);
   }
 
-  public static <P extends Data<P>> DataParser<P, String> bcd(String id, int length) {
+  public static <P extends Data<P>> StringParser<P> bcd(String id, int length) {
     return new BcdStringParser<>(id, length);
   }
 }
