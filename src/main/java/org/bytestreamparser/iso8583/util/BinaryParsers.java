dcce8d1cf363cdbd129f81bb89346a038a54d532
Lin Yang <coding@lyang.dev>
2025-01-31 20:05:09 -0800

    Rename parser utils methods (#31)
    * Rename BinaryParsers methods
    
    * Rename more parser util methods

--- src/main/java/org/bytestreamparser/iso8583/util/BinaryParsers.java
+++ src/main/java/org/bytestreamparser/iso8583/util/BinaryParsers.java
@@ -11,36 +11,36 @@ import org.bytestreamparser.scalar.parser.BinaryParser;
 public final class BinaryParsers {
   private BinaryParsers() {}
 
-  public static DataParser<byte[]> bin(String id, int length) {
+  public static DataParser<byte[]> fixedLength(String id, int length) {
     return new BinaryParser(id, length);
   }
 
-  public static DataParser<byte[]> ubyteBin(String id) {
-    return varBin(id, IntegerParsers.ubyte(id));
+  public static DataParser<byte[]> binaryLVar(String id) {
+    return variableLength(id, IntegerParsers.ubyte(id));
   }
 
-  public static DataParser<byte[]> ushortBin(String id) {
-    return varBin(id, IntegerParsers.ushort(id));
+  public static DataParser<byte[]> binaryLLVar(String id) {
+    return variableLength(id, IntegerParsers.ushort(id));
   }
 
-  public static DataParser<byte[]> llBin(String id) {
-    return varBin(id, IntegerParsers.plain(id, 2));
+  public static DataParser<byte[]> textLLVar(String id) {
+    return variableLength(id, IntegerParsers.text(id, 2));
   }
 
-  public static DataParser<byte[]> lllBin(String id) {
-    return varBin(id, IntegerParsers.plain(id, 3));
+  public static DataParser<byte[]> textLLLVar(String id) {
+    return variableLength(id, IntegerParsers.text(id, 3));
   }
 
-  public static DataParser<byte[]> varBin(String id, DataParser<Integer> lengthParser) {
+  public static DataParser<byte[]> variableLength(String id, DataParser<Integer> lengthParser) {
     return new VariableLengthParser<>(
-        id, lengthParser, length -> bin(id, length), bytes -> bytes.length);
+        id, lengthParser, length -> fixedLength(id, length), bytes -> bytes.length);
   }
 
-  public static DataParser<FixedBitmap> bmp(String id, int bytes) {
+  public static DataParser<FixedBitmap> fixedBitmap(String id, int bytes) {
     return new FixedBitmapParser(id, bytes);
   }
 
-  public static DataParser<ExtendableBitmap> ebmp(String id, int bytes) {
+  public static DataParser<ExtendableBitmap> extendableBitmap(String id, int bytes) {
     return new ExtendableBitmapParser(id, bytes);
   }
 }
--- src/test/java/org/bytestreamparser/iso8583/util/BinaryParsersTest.java
+++ src/test/java/org/bytestreamparser/iso8583/util/BinaryParsersTest.java
@@ -19,16 +19,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 class BinaryParsersTest {
 
   @Test
-  void bin(@Randomize byte[] value) throws IOException {
-    DataParser<byte[]> parser = BinaryParsers.bin("id", value.length);
+  void fixedLength(@Randomize byte[] value) throws IOException {
+    DataParser<byte[]> parser = BinaryParsers.fixedLength("fixed", value.length);
     ByteArrayInputStream input = new ByteArrayInputStream(value);
     assertThat(parser.parse(input)).isEqualTo(value);
   }
 
   @Test
-  void ubyte_bin(@Randomize(length = 10) byte[] value) throws IOException {
+  void binaryLVar(@Randomize(length = 10) byte[] value) throws IOException {
     value[0] = 4;
-    DataParser<byte[]> parser = BinaryParsers.ubyteBin("ubyte-bin");
+    DataParser<byte[]> parser = BinaryParsers.binaryLVar("binaryLVar");
     ByteArrayInputStream input = new ByteArrayInputStream(value);
 
     assertThat(parser.parse(input)).isEqualTo(Arrays.copyOfRange(value, 1, 5));
@@ -43,42 +43,44 @@ class BinaryParsersTest {
   }
 
   @Test
-  void ushort_bin(@Randomize(length = 10) byte[] value) throws IOException {
+  void binaryLLVar(@Randomize(length = 10) byte[] value) throws IOException {
     value[0] = 0;
     value[1] = 3;
-    DataParser<byte[]> parser = BinaryParsers.ushortBin("ushort-bin");
+    DataParser<byte[]> parser = BinaryParsers.binaryLLVar("binaryLLVar");
     ByteArrayInputStream input = new ByteArrayInputStream(value);
     assertThat(parser.parse(input)).isEqualTo(Arrays.copyOfRange(value, 2, 5));
     assertThat(input.available()).isEqualTo(5);
   }
 
   @Test
-  void ll_bin(@Randomize(intMin = 1, intMax = 10) int length, @Randomize(length = 10) byte[] value)
+  void textLLVar(
+      @Randomize(intMin = 1, intMax = 10) int length, @Randomize(length = 10) byte[] value)
       throws IOException {
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     output.write(String.format("%02d", length).getBytes());
     output.write(value);
-    DataParser<byte[]> parser = BinaryParsers.llBin("ll-bin");
+    DataParser<byte[]> parser = BinaryParsers.textLLVar("plainLLVar");
     ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
     assertThat(parser.parse(input)).isEqualTo(Arrays.copyOfRange(value, 0, length));
     assertThat(input.available()).isEqualTo(value.length - length);
   }
 
   @Test
-  void lll_bin(@Randomize(intMin = 1, intMax = 10) int length, @Randomize(length = 10) byte[] value)
+  void textLLLVar(
+      @Randomize(intMin = 1, intMax = 10) int length, @Randomize(length = 10) byte[] value)
       throws IOException {
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     output.write(String.format("%03d", length).getBytes());
     output.write(value);
-    DataParser<byte[]> parser = BinaryParsers.lllBin("lll-bin");
+    DataParser<byte[]> parser = BinaryParsers.textLLLVar("plainLLLVar");
     ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
     assertThat(parser.parse(input)).isEqualTo(Arrays.copyOfRange(value, 0, length));
     assertThat(input.available()).isEqualTo(value.length - length);
   }
 
   @Test
-  void bmp(@Randomize byte[] value) throws IOException {
-    DataParser<FixedBitmap> parser = BinaryParsers.bmp("bitmap", value.length);
+  void fixedBitmap(@Randomize byte[] value) throws IOException {
+    DataParser<FixedBitmap> parser = BinaryParsers.fixedBitmap("fixedBitmap", value.length);
     ByteArrayInputStream input = new ByteArrayInputStream(value);
     FixedBitmap parsed = parser.parse(input);
     FixedBitmap expected = FixedBitmap.valueOf(value);
@@ -89,12 +91,13 @@ class BinaryParsersTest {
   }
 
   @Test
-  void ebmp(@Randomize(length = 10) byte[] value) throws IOException {
+  void extendableBitmap(@Randomize(length = 10) byte[] value) throws IOException {
     value[0] = (byte) (0b10000000 | value[0]);
     value[value.length / 2] = (byte) (0b01111111 & value[value.length / 2]);
     value[value.length / 2] = (byte) (0b00000001 | value[value.length / 2]);
     ByteArrayInputStream input = new ByteArrayInputStream(value);
-    DataParser<ExtendableBitmap> parser = BinaryParsers.ebmp("bitmap", value.length / 2);
+    DataParser<ExtendableBitmap> parser =
+        BinaryParsers.extendableBitmap("extendableBitmap", value.length / 2);
     ExtendableBitmap parsed = parser.parse(input);
     char[] chars = TestHelper.toBinaryString(value).toCharArray();
     for (int index = 0; index < chars.length; index++) {
